9b5efc770dc003f4daf9b88ea78725ba87674c33
diplodoc13 <5286606m@gmail.com>
2022-07-30 14:36:27 +0700

    first commit

---
+++ bboard/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from main.views import index, other_page
+
+app_name = "main"
+urlpatterns = [
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index'),
+]
